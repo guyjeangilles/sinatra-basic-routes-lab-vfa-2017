a8d0cc9bdc5d9b6c129a46df5ad1608d6266997f
Guyrandy Jean-Gilles <subscriptions.gjgilles@protonmail.com>
2017-08-01 21:07:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #name
+  get '/name' do
+    "My name is Enigo Montoya"
+  end
+  #hometown
+  get '/hometown' do
+    "My hometown is Hard Knox"
+  end
+  #favorite-song
+  get '/favorite-song' do
+    "My favorite song is 'Prepare To Die'"
+  end
 end
